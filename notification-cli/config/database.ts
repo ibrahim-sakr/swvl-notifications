b8c9bbc46c82406dc3a87f6fa59147abfd91f8d0
Ibrahim Sakr <ebrahimes@gmail.com>
2019-04-30 13:00:42 +0200

    fixes

--- notification-cli/config/database.ts
+++ notification-cli/config/database.ts
@@ -1,6 +1,5 @@
-
 export const database = {
-    host: process.env.DB_HOST || 'mongo',
+    host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 27017,
     name: process.env.DB_NAME || 'swvl_notifications',
 }
--- notification-cli/config/rabbitmq.ts
+++ notification-cli/config/rabbitmq.ts
@@ -1,4 +1,4 @@
 export const rabbitmq = {
-    host: process.env.RABBITMQ_HOST || 'rabbitmq',
+    host: process.env.RABBITMQ_HOST || 'localhost',
     queue: process.env.RABBITMQ_QUEUE || 'notificationQueue'
 }
